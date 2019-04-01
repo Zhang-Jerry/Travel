34977f6c46d1f06dd70265274ebf08145e58c9ff
Zhang-Jerry <z_jerry2016@163.com>
2019-04-01 23:41:14 +0800

    init project

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 解决部分手机机型上部分浏览器存在300ms的click延时问题
+import fastClick from 'fastclick'
+// 样式初始化，解决浏览器样式差异
+import './assets/styles/reset.css'
+// 解决移动端多倍屏将1px边框显示成多像素边框问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
